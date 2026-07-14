56fba6cd9a6a929dadd2d95f520d7d6f41628bb7
mimishhka <mireillel041@gmail.com>
2026-07-14 13:44:17 -0400

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,6 +25,7 @@ import Lab from "./pages/Lab";
 import Compliance from "./pages/Compliance";
 import Privacy from "./pages/Privacy";
 import Faq from "./pages/Faq";
+import NotFound from "./pages/NotFound";
 import Admin from "./pages/admin/AdminLayout";
 
 import "./index.css";
@@ -68,7 +69,7 @@ function AppRoutes() {
       <Route path="/privacy" element={<Privacy />} />
       <Route path="/faq" element={<Faq />} />
       <Route path="/admin/*" element={<ProtectedRoute adminOnly><Admin /></ProtectedRoute>} />
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
